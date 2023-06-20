50ad20f49c6468801a5a4a6e52fbb46c4046e1f3
naughtyGitCat <psyduck007@outlook.com>
2023-06-20 20:38:15 +0800

    first demo

--- config.py
+++ config.py
@@ -3,19 +3,28 @@ import dataclasses
 
 
 class Config:
-    ffmpeg = r"C:\Program Files\FFmpeg\bin\ffmpeg.exe"
-    audio_tmp_path = r"F:\audio_tmp"
 
-    model = r"E:\wisper\models\medium.pt"
-
-    # media_path = r"E:\Something\英雄时刻"
-    media_path = r"\\B350m\e\电影\纪录片\国家地理.伟大工程巡礼系列National.Geographic.Megastructures.720p.HDTV"
+    # media_path = r"\\B350m\e\电影\纪录片\国家地理.伟大工程巡礼系列National.Geographic.Megastructures.720p.HDTV"
+    media_path = r"\\Gen8\d\RAW\STARS-250"
     media_suffix = ["mp4", "mkv", "wmv", 'avi']
     media_smb_user = ""
     media_smb_password = ""
-    search_recursive = True
+    media_search_recursive = True
+
+    ffmpeg = r"C:\Program Files\FFmpeg\bin\ffmpeg.exe"
+    audio_tmp_path = r"F:\audio_tmp"
+
+    # for more detail, please read github.com/openai/whisper/__init__.py,transcribe.py
+    whisper_model_path = r"E:\wisper\models"  # where to find the models, where to place the models when download
+    whisper_model_name = "medium"  # tiny[.en], small[.en], medium[.en], large. for detail, please read README.md
+    whisper_model_device = 'cuda'  # cuda,cpu,None(without quotes). use None to let whisper auto choose cuda or cpu
+
     translate = True
     translate_to = "zh"
+    translate_api = ["bing", "VolcEngine", "Sogou", "Baidu", "Iflytek", "Yandex"]  # https://github.com/UlionTse/translators
+    translate_fail_action = ""  # use '' or 'translation failed'
 
-    overwrite = True
+    srt_overwrite = True  # overwrite the existence srt
+    srt_bilingual = True  # translated words in first line, while the origin lying on second line
+
     verbose = True
--- main.py
+++ main.py
@@ -1,34 +1,54 @@
-import os
+import datetime
 import pathlib
+import pprint
 
 import whisper
-import video2audio
 import config
-from file import get_files, remove_file
-import pprint
-from whisper.utils import get_writer
+from utils.file import get_files, remove_file
+from utils import srt_writer, video2audio, transcriber
+
+
+def get_srt_filepath(video_file_fullpath: str) -> str:
+    vf = pathlib.Path(video_file_fullpath)
+    return str(vf.parent.joinpath(f"{vf.stem}.srt"))
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('get files')
-    files = get_files(config.Config.media_path, config.Config.search_recursive)
+    files = get_files(config.Config.media_path, config.Config.media_search_recursive)
 
     model: whisper.Whisper
     model_loaded: bool = False
     for video_file in files:
         if not model_loaded:
             print('load model')
-            model = whisper.load_model(config.Config.model)
+            model = whisper.load_model(name=config.Config.whisper_model_name,
+                                       device=config.Config.whisper_model_device,
+                                       download_root=config.Config.whisper_model_path)
             model_loaded = True
         print(f'do file: {video_file}')
         audio_file = video2audio.video2audio(video_file)
 
         print(f'now transcribe {audio_file}')
-        result = model.transcribe(audio_file, language=None, verbose=config.Config.verbose)
-        remove_file(audio_file)
-        writer = get_writer('srt', str(pathlib.Path(video_file).parent))
-        writer(result, video_file)
-        print(pprint.pprint(result))
-        break
+        srt_path = get_srt_filepath(video_file)
+        # result = model.transcribe(audio_file, language=None, verbose=config.Config.verbose)
+        # print(f'[DEBUG] {pprint.pprint(result)}')
+        # remove_file(audio_file)
+        # writer = srt_writer.get_srt_writer(str(pathlib.Path(video_file).parent))
+        # writer(result, video_file)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        writer = srt_writer.SRTWriter(srt_path)
+        ts = datetime.datetime.now()
+        segments = transcriber.transcribe_to_segments(model,
+                                                      audio_file,
+                                                      language=None,
+                                                      verbose=config.Config.verbose)
+        for segment in segments:
+            writer.segment_to_srt1(segment)
+
+        print(f'srt file path: {srt_path}')
+        print(f'srt file for {video_file} generated')
+        te = datetime.datetime.now()
+        print(f"[DEBUG] {(te-ts).total_seconds()}")
+        break
