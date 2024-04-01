3b058d8041501e2fd6b3831d27e560d5a959d29d
naughtyGitCat <psyduck007@outlook.com>
2024-04-01 15:29:05 +0800

    fix config

--- utils/translator.py
+++ utils/translator.py
@@ -2,14 +2,14 @@
 import traceback
 import translators
 
-from utils import config
+from utils import CONFIG
 
 
-def translate(line: str, source_lang: str = 'auto', target_lang: str = config.CONFIG.Translate.target_language) -> str:
-    result = config.CONFIG.Translate.fail_hint
+def translate(line: str, source_lang: str = 'auto', target_lang: str = CONFIG.Translate.target_language) -> str:
+    result = CONFIG.Translate.fail_hint
     try:
         result = translators.translate_text(line,
-                                            translator=config.CONFIG.Translate.api,
+                                            translator=CONFIG.Translate.api,
                                             from_language=source_lang,
                                             to_language=target_lang)
         if isinstance(result, str):
