48c547f42cbe6cc2fbbf155b0fb3f252b9a18f9d
naughtyGitCat <psyduck007@outlook.com>
2024-04-01 15:29:06 +0800

    improve linux access experience

--- utils/video2audio.py
+++ utils/video2audio.py
@@ -2,11 +2,9 @@
 import os.path
 import pathlib
 import subprocess
-
+import platform
 import config
-
-FFMPEG = r"C:\Program Files\FFmpeg\bin\ffmpeg.exe"
-
+from utils.file import (get_file_name, get_path_parent, join_path)
 
 def video2audio(video_file: str) -> str:
     """
@@ -16,18 +14,19 @@ def video2audio(video_file: str) -> str:
     """
     print(f'[DEBUG]now video2audio {video_file}')
     if config.Config.audio_tmp_path == "":
-        _audio_tmp_path = pathlib.Path(video_file).parent
+        _audio_tmp_path = get_path_parent(video_file)
     else:
         _audio_tmp_path = config.Config.audio_tmp_path
-    video_file_name = pathlib.Path(video_file).name
+    video_file_name = get_file_name(video_file)
     print(f'[DEBUG]video_file_name {video_file_name}')
-    audio_file = os.path.join(_audio_tmp_path, f"{video_file_name}.wisper.wav")
+    audio_file = join_path(_audio_tmp_path, f"{video_file_name}.wisper.wav")
     print(f'[DEBUG]audio_file {audio_file}')
-    p = subprocess.Popen([FFMPEG, "-i", video_file,
+    p = subprocess.Popen([config.Config.ffmpeg, "-i", video_file,
                           "-f", "wav", "-vn", "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1",
                           # "-ss", "00:00:00", "-to", "00:01:10",  # for test, only convert the header fragment audio
                           "-y", audio_file],
                          stdout=subprocess.PIPE, encoding="utf-8")
+    print(f'[DEBUG] {subprocess.list2cmdline(p.args)}')
     while True:
         output = p.stdout.readline()
         if p.poll() is not None and output == '':
