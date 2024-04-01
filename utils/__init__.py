d0288bf7815ab0e06e96fa53e241e9becedf7784
naughtyGitCat <psyduck007@outlook.com>
2024-04-01 15:29:06 +0800

    debug import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,3 @@
-from utils.config import CONFIG, init_config
+from utils.config import CONFIG
 from utils.logger import get_logger
 from utils.file import get_files
-
-if CONFIG is None:
-    CONFIG = init_config()
