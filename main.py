2db160ba885fe31b194596af2521e75e0152a809
naughtyGitCat <psyduck007@outlook.com>
2024-04-01 15:29:06 +0800

    use toml instead of py config, add multi target support

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import datetime
+import logging
 import pathlib
 
 import whisper
 from utils.file import get_files
-from utils import srt_writer, video2audio, transcriber, config
+from utils import srt_writer, video2audio, transcriber
+from utils.config import CONFIG
+from utils.logger import get_logger
+
+logger = get_logger("main")
 
 
 def get_srt_filepath(video_file_fullpath: str) -> str:
@@ -13,39 +18,39 @@ def get_srt_filepath(video_file_fullpath: str) -> str:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('get files')
-    files = get_files(config.Config.media_path, config.Config.media_search_recursive)
+    logger.info('get files')
+    for target in CONFIG.Targets:
+        files = get_files(target.path, target.search_recursive)
+        model: whisper.Whisper
+        model_loaded: bool = False
+        for video_file in files:
+            if not model_loaded:
+                logger.info('load model')
+                model = whisper.load_model(name=CONFIG.Whisper.model_name,
+                                           device=CONFIG.Whisper.model_device,
+                                           download_root=CONFIG.Whisper.model_path)
+                model_loaded = True
+            logger.info(f'do file: {video_file}')
+            audio_file = video2audio.video2audio(video_file)
 
-    model: whisper.Whisper
-    model_loaded: bool = False
-    for video_file in files:
-        if not model_loaded:
-            print('load model')
-            model = whisper.load_model(name=config.Config.whisper_model_name,
-                                       device=config.Config.whisper_model_device,
-                                       download_root=config.Config.whisper_model_path)
-            model_loaded = True
-        print(f'do file: {video_file}')
-        audio_file = video2audio.video2audio(video_file)
+            logger.info(f'now transcribe {audio_file}')
+            srt_path = get_srt_filepath(video_file)
+            # result = model.transcribe(audio_file, language=None, verbose=config.Config.verbose)
+            # print(f'[DEBUG] {pprint.pprint(result)}')
+            # remove_file(audio_file)
+            # writer = srt_writer.get_srt_writer(str(pathlib.Path(video_file).parent))
+            # writer(result, video_file)
 
-        print(f'now transcribe {audio_file}')
-        srt_path = get_srt_filepath(video_file)
-        # result = model.transcribe(audio_file, language=None, verbose=config.Config.verbose)
-        # print(f'[DEBUG] {pprint.pprint(result)}')
-        # remove_file(audio_file)
-        # writer = srt_writer.get_srt_writer(str(pathlib.Path(video_file).parent))
-        # writer(result, video_file)
+            writer = srt_writer.SRTWriter(srt_path)
+            ts = datetime.datetime.now()
+            segments = transcriber.transcribe_to_segments(model,
+                                                          audio_file,
+                                                          language=None,
+                                                          verbose=CONFIG.Log.level == logging.DEBUG)
+            for segment in segments:
+                writer.segment_to_srt1(segment)
 
-        writer = srt_writer.SRTWriter(srt_path)
-        ts = datetime.datetime.now()
-        segments = transcriber.transcribe_to_segments(model,
-                                                      audio_file,
-                                                      language=None,
-                                                      verbose=config.Config.verbose)
-        for segment in segments:
-            writer.segment_to_srt1(segment)
-
-        print(f'srt file path: {srt_path}')
-        print(f'srt file for {video_file} generated')
-        te = datetime.datetime.now()
-        print(f"[DEBUG] transcribe and translate to srt cost: {(te-ts).total_seconds()} seconds")
+            logger.info(f'srt file path: {srt_path}')
+            logger.info(f'srt file for {video_file} generated')
+            te = datetime.datetime.now()
+            logger.debug(f"[DEBUG] transcribe and translate to srt cost: {(te-ts).total_seconds()} seconds")
