50cc6431cb817c4d8c0d89e52434ae62e00860a1
naughtyGitCat <psyduck007@outlook.com>
2024-04-01 15:29:06 +0800

    debug import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
-from utils.config import CONFIG, init_config
-from utils.logger import get_logger
-from utils.file import get_files
+from .config import CONFIG, init_config
+from .logger import get_logger
+from .file import get_files
 
 init_config()
